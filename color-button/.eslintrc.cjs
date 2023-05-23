a2f1026daed4d675f2c4ddc4e8e9f3b21d0bcbbc
shitakemura <shitakemura@gmail.com>
2023-05-23 17:21:43 +0900

    Add eslint plugins for testing-library and js-dom

--- color-button/.eslintrc.cjs
+++ color-button/.eslintrc.cjs
@@ -1,15 +1,17 @@
 module.exports = {
   env: { browser: true, es2020: true, node: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+    'plugin:testing-library/react',
+    'plugin:jest-dom/recommended',
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  plugins: ["react-refresh"],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh', 'testing-library', 'jest-dom'],
   rules: {
-    "react-refresh/only-export-components": "warn",
+    'react-refresh/only-export-components': 'warn',
   },
-};
+}
--- color-button/README.md
+++ color-button/README.md
@@ -4,6 +4,7 @@
 
 - npm install -D jest @types/jest ts-jest ts-node jest-environment-jsdom
 - npm install -D @testing-library/jest-dom @testing-library/react @testing-library/user-event
+- npm install -D eslint-plugin-testing-library eslint-plugin-jest-dom
 
 - getByRole()
 - toBeEnabled() / toBeDisabled()
