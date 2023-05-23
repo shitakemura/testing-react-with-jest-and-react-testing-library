4cd15d0e071534b04e0ac977fd729fbff8bb0f1f
shitakemura <shitakemura@gmail.com>
2023-05-24 01:34:33 +0900

    Add popover to SummaryForm

--- sundaes-on-demand/src/pages/summary/SummaryForm.test.tsx
+++ sundaes-on-demand/src/pages/summary/SummaryForm.test.tsx
@@ -1,10 +1,10 @@
-import { fireEvent, render, screen, logRoles } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { SummaryForm } from './SummaryForm'
 
 describe('SummaryForm', () => {
   test('Initial condition', () => {
-    const { container } = render(<SummaryForm />)
-    logRoles(container)
+    render(<SummaryForm />)
 
     const checkbox = screen.getByRole('checkbox', {
       name: 'I agree to Terms and Conditions',
@@ -15,16 +15,41 @@ describe('SummaryForm', () => {
     expect(confirmButton).toBeDisabled()
   })
 
-  test('Checkbox disables button on first click and enables on second click', () => {
+  test('Checkbox disables button on first click and enables on second click', async () => {
+    const user = userEvent.setup()
+
     render(<SummaryForm />)
 
     const checkbox = screen.getByRole('checkbox')
     const confirmButton = screen.getByRole('button', { name: 'Confirm order' })
 
-    fireEvent.click(checkbox)
+    await user.click(checkbox)
     expect(confirmButton).toBeEnabled()
 
-    fireEvent.click(checkbox)
+    await user.click(checkbox)
     expect(confirmButton).toBeDisabled()
+  })
+
+  test('popover responds to hover', async () => {
+    const user = userEvent.setup()
+
+    render(<SummaryForm />)
+
+    // popover starts out hidden
+    const nullPopover = screen.queryByText(
+      'No ice cream will actually be delivered'
+    )
+    expect(nullPopover).not.toBeInTheDocument()
+
+    // popover appears on mouseover of checkbox label
+    const termsOfConditions = screen.getByText('Terms and Conditions')
+    await user.hover(termsOfConditions)
+
+    const popover = screen.getByText('No ice cream will actually be delivered')
+    expect(popover).toBeInTheDocument()
+
+    // popover disappears when we mouse out
+    await user.unhover(termsOfConditions)
+    expect(popover).not.toBeInTheDocument()
   })
 })
--- sundaes-on-demand/src/pages/summary/SummaryForm.tsx
+++ sundaes-on-demand/src/pages/summary/SummaryForm.tsx
@@ -2,13 +2,24 @@ import { useState } from 'react'
 
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Popover from 'react-bootstrap/Popover'
 
 export function SummaryForm() {
   const [tcChecked, setTcChecked] = useState(false)
 
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  )
+
   const checkboxLabel = (
     <span>
-      I agree to <span style={{ color: 'blue' }}>Terms and Conditions</span>
+      I agree to
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: 'blue' }}> Terms and Conditions</span>
+      </OverlayTrigger>
     </span>
   )
 
