a3fdde5ad4f857c7f18492a989acc1120b6b686c
shitakemura <shitakemura@gmail.com>
2023-05-24 17:18:54 +0900

    Implement toppings subtotal

--- sundaes-on-demand/src/pages/entry/ToppingOption.tsx
+++ sundaes-on-demand/src/pages/entry/ToppingOption.tsx
@@ -1,14 +1,25 @@
 import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
 import { OptionItem } from '../../types/OptionType'
+import { useOrderDetails } from '../../contexts/OrderDetails'
 
 export function ToppingOption({ name, imagePath }: OptionItem) {
+  const { updateItemCount } = useOrderDetails()
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const itemCount = event.target.checked ? 1 : 0
+    updateItemCount(name, itemCount, 'toppings')
+  }
+
   return (
-    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
+    <Col xs={6} sm={4} md={3} lg={2} style={{ textAlign: 'center' }}>
       <img
         style={{ width: '75%' }}
         src={`${imagePath}`}
         alt={`${name} topping`}
       />
+      <Form.Group controlId={`${name}-topping-checkbox`}>
+        <Form.Check type="checkbox" onChange={handleChange} label={name} />
+      </Form.Group>
     </Col>
   )
 }
--- sundaes-on-demand/src/pages/entry/TotalUpdates.test.tsx
+++ sundaes-on-demand/src/pages/entry/TotalUpdates.test.tsx
@@ -25,3 +25,34 @@ test('update scoop subtotal when scoops change', async () => {
   await user.type(chocolateInput, '2')
   expect(scoopsSubtotal).toHaveTextContent('6.00')
 })
+
+test('update topping subtotal when toppings change', async () => {
+  const user = userEvent.setup()
+
+  render(<Options optionType="toppings" />)
+
+  // check initial subtotal
+  const toppingsSubtotal = screen.getByText('Toppings total: $', {
+    exact: false,
+  })
+  expect(toppingsSubtotal).toHaveTextContent('0.00')
+
+  // add cherries and check subtotal
+  const cherriesInput = await screen.findByRole('checkbox', {
+    name: 'Cherries',
+  })
+  await user.click(cherriesInput)
+  expect(cherriesInput).toBeChecked()
+  expect(toppingsSubtotal).toHaveTextContent('1.50')
+
+  // add m&m and check subtotal
+  const mmInput = await screen.findByRole('checkbox', { name: 'M&Ms' })
+  await user.click(mmInput)
+  expect(mmInput).toBeChecked()
+  expect(toppingsSubtotal).toHaveTextContent('3.00')
+
+  // remove m&m and check subtotal
+  await user.click(mmInput)
+  expect(mmInput).not.toBeChecked()
+  expect(toppingsSubtotal).toHaveTextContent('1.50')
+})
